43128be983e05db5c06789b93bc1751b344b4074
Eyna Kane <eyna.kane@gmail.com>
2019-03-17 21:33:57 -0400

    Fix rubocop offense

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -23,7 +23,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
         user: {
           first_name: Faker::Name.first_name,
           last_name: Faker::Name.last_name,
-          username: Faker::Internet.user_name ,
+          username: Faker::Internet.user_name,
           email: Faker::Internet.email,
           password: password,
           password_confirmation: password
